a9738bd4d2068a04600f2d02519c804b33c456de
moiseev <alexey@moiseev.pro>
2026-02-28 15:33:19 -0300

    fix backup.sh

--- backup.sh
+++ backup.sh
@@ -4,11 +4,11 @@ DATE=$(date +%Y%m%d_%H%M%S)
 BACKUP_DIR="/opt/chat_bot_mac/backups"
 mkdir -p $BACKUP_DIR
 
-docker cp $(docker-compose ps -q django):/app/db.sqlite3 $BACKUP_DIR/db_$DATE.sqlite3
+docker cp $(docker compose ps -q django):/app/db.sqlite3 $BACKUP_DIR/db_$DATE.sqlite3
 
 redis-cli --rdb $BACKUP_DIR/redis_$DATE.rdb
 
-docker-compose logs > $BACKUP_DIR/logs_$DATE.log
+docker compose logs > $BACKUP_DIR/logs_$DATE.log
 
 find $BACKUP_DIR -name "db_*.sqlite3" -mtime +30 -delete
 find $BACKUP_DIR -name "redis_*.rdb" -mtime +30 -delete
